1a7d37892e704addb1e1a8d72bc64a7a5ce0feb5
Jared Gorski <jared.gorski@liferay.com>
2018-04-05 14:38:38 -0700

    Add clickout of menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Behavior when nav menu opened
 window.onload = function() {
-  document.getElementById("burgerTog").addEventListener("click",
-  function stopScroll() {
-    const menuTog = document.getElementById("burgerTog");
+  const menuTog = document.getElementById("burgerTog");
+  let weFade = document.getElementsByClassName("canFade");
+  let weClear = document.getElementsByClassName("canClear");
+  let weLeave = document.getElementsByClassName("canLeave");
+  let x;
 
-    let weFade = document.getElementsByClassName("canFade");
-    let weClear = document.getElementsByClassName("canClear");
-    let weLeave = document.getElementsByClassName("canLeave");
-    let x;
+  document.getElementById("burgerTog").addEventListener("click",
+  function navOpen() {
     if (menuTog.checked == true) {
       document.body.classList.add("noScroll");
       document.getElementById("navOverlay").classList.add("navOpenOverlay");
@@ -37,5 +37,26 @@ window.onload = function() {
         };
       }, 240);
     };
+  });
+  document.getElementById("navOverlay").addEventListener("click",
+  function clickOut() {
+    if (menuTog.checked == true) {
+      menuTog.checked = false;
+
+      document.body.classList.remove("noScroll");
+      document.getElementById("navOverlay").classList.remove("navOpenOverlay");
+
+      for (x = 0; x < weFade.length; x++) {
+        weFade[x].classList.remove("fadeOut");
+      }
+      for (x = 0; x < weClear.length; x++) {
+        weClear[x].classList.remove("clearOut");
+      }
+      setTimeout(() => {
+        for (x = 0; x < weLeave.length; x++) {
+          weLeave[x].classList.remove("iLeave");
+        };
+      }, 240);
+    }
   });
 };
