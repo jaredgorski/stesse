56adce00f48d6b8e9bb20ed0131512ba1626b723
Jared Gorski <jared.gorski@liferay.com>
2018-04-11 16:31:58 -0700

    Test ios scrolling

--- hosting/js/main.js
+++ hosting/js/main.js
@@ -1,68 +1,66 @@
 // Behavior when nav menu opened and closed
-window.onload = function() {
-  const menuTog = document.getElementById("burgerTog");
-  let weFade = document.getElementsByClassName("canFade");
-  let weClear = document.getElementsByClassName("canClear");
-  let weLeave = document.getElementsByClassName("canLeave");
-  let x;
+const menuTog = document.getElementById("burgerTog");
+let weFade = document.getElementsByClassName("canFade");
+let weClear = document.getElementsByClassName("canClear");
+let weLeave = document.getElementsByClassName("canLeave");
+let x;
 
-  document.getElementById("burgerTog").addEventListener("click",
-  function navOpen() {
-    if (menuTog.checked == true) {
-      document.body.classList.remove("parallax-bodyscroll");
-      document.body.classList.add("noScroll");
-      document.getElementById("navOverlay").classList.add("navOpenOverlay");
+document.getElementById("burgerTog").addEventListener("click",
+function navOpen() {
+  if (menuTog.checked == true) {
+    document.body.classList.remove("parallax-bodyscroll");
+    document.body.classList.add("noScroll");
+    document.getElementById("navOverlay").classList.add("navOpenOverlay");
 
-      for (x = 0; x < weFade.length; x++) {
-        weFade[x].classList.add("fadeOut");
-      }
-      for (x = 0; x < weClear.length; x++) {
-        weClear[x].classList.add("clearOut");
-      }
-      for (x = 0; x < weLeave.length; x++) {
-        weLeave[x].classList.add("iLeave");
-      }
-    } else {
-      document.body.classList.add("parallax-bodyscroll");
-      document.body.classList.remove("noScroll");
-      document.getElementById("navOverlay").classList.remove("navOpenOverlay");
-
-      for (x = 0; x < weFade.length; x++) {
-        weFade[x].classList.remove("fadeOut");
-      }
-      for (x = 0; x < weClear.length; x++) {
-        weClear[x].classList.remove("clearOut");
-      }
-      setTimeout(() => {
-        for (x = 0; x < weLeave.length; x++) {
-          weLeave[x].classList.remove("iLeave");
-        };
-      }, 240);
-    };
-  });
-  document.getElementById("navOverlay").addEventListener("click",
-  function clickOut() {
-    if (menuTog.checked == true) {
-      menuTog.checked = false;
-
-      document.body.classList.add("parallax-bodyscroll");
-      document.body.classList.remove("noScroll");
-      document.getElementById("navOverlay").classList.remove("navOpenOverlay");
-
-      for (x = 0; x < weFade.length; x++) {
-        weFade[x].classList.remove("fadeOut");
-      }
-      for (x = 0; x < weClear.length; x++) {
-        weClear[x].classList.remove("clearOut");
-      }
-      setTimeout(() => {
-        for (x = 0; x < weLeave.length; x++) {
-          weLeave[x].classList.remove("iLeave");
-        };
-      }, 240);
+    for (x = 0; x < weFade.length; x++) {
+      weFade[x].classList.add("fadeOut");
     }
-  });
-};
+    for (x = 0; x < weClear.length; x++) {
+      weClear[x].classList.add("clearOut");
+    }
+    for (x = 0; x < weLeave.length; x++) {
+      weLeave[x].classList.add("iLeave");
+    }
+  } else {
+    document.body.classList.add("parallax-bodyscroll");
+    document.body.classList.remove("noScroll");
+    document.getElementById("navOverlay").classList.remove("navOpenOverlay");
+
+    for (x = 0; x < weFade.length; x++) {
+      weFade[x].classList.remove("fadeOut");
+    }
+    for (x = 0; x < weClear.length; x++) {
+      weClear[x].classList.remove("clearOut");
+    }
+    setTimeout(() => {
+      for (x = 0; x < weLeave.length; x++) {
+        weLeave[x].classList.remove("iLeave");
+      };
+    }, 240);
+  };
+});
+document.getElementById("navOverlay").addEventListener("click",
+function clickOut() {
+  if (menuTog.checked == true) {
+    menuTog.checked = false;
+
+    document.body.classList.add("parallax-bodyscroll");
+    document.body.classList.remove("noScroll");
+    document.getElementById("navOverlay").classList.remove("navOpenOverlay");
+
+    for (x = 0; x < weFade.length; x++) {
+      weFade[x].classList.remove("fadeOut");
+    }
+    for (x = 0; x < weClear.length; x++) {
+      weClear[x].classList.remove("clearOut");
+    }
+    setTimeout(() => {
+      for (x = 0; x < weLeave.length; x++) {
+        weLeave[x].classList.remove("iLeave");
+      };
+    }, 240);
+  }
+});
 
 
 
