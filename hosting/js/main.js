a378ab8ceaa89e2da2aae26a945b5b94bdb9e339
Jared Gorski <jared.gorski@liferay.com>
2018-04-12 12:17:05 -0700

    Test ios scrolling

--- hosting/js/main.js
+++ hosting/js/main.js
@@ -3,7 +3,7 @@ const menuTog = document.getElementById("burgerTog");
 let weFade = document.getElementsByClassName("canFade");
 let weClear = document.getElementsByClassName("canClear");
 let weLeave = document.getElementsByClassName("canLeave");
-let x;
+let classInstance;
 
 document.getElementById("burgerTog").addEventListener("click",
 function navOpen() {
@@ -12,29 +12,29 @@ function navOpen() {
     document.body.classList.add("noScroll");
     document.getElementById("navOverlay").classList.add("navOpenOverlay");
 
-    for (x = 0; x < weFade.length; x++) {
-      weFade[x].classList.add("fadeOut");
+    for (classInstance = 0; classInstance < weFade.length; classInstance++) {
+      weFade[classInstance].classList.add("fadeOut");
     }
-    for (x = 0; x < weClear.length; x++) {
-      weClear[x].classList.add("clearOut");
+    for (classInstance = 0; classInstance < weClear.length; classInstance++) {
+      weClear[classInstance].classList.add("clearOut");
     }
-    for (x = 0; x < weLeave.length; x++) {
-      weLeave[x].classList.add("iLeave");
+    for (classInstance = 0; classInstance < weLeave.length; classInstance++) {
+      weLeave[classInstance].classList.add("iLeave");
     }
   } else {
     document.body.classList.add("parallax-bodyscroll");
     document.body.classList.remove("noScroll");
     document.getElementById("navOverlay").classList.remove("navOpenOverlay");
 
-    for (x = 0; x < weFade.length; x++) {
-      weFade[x].classList.remove("fadeOut");
+    for (classInstance = 0; classInstance < weFade.length; classInstance++) {
+      weFade[classInstance].classList.remove("fadeOut");
     }
-    for (x = 0; x < weClear.length; x++) {
-      weClear[x].classList.remove("clearOut");
+    for (classInstance = 0; classInstance < weClear.length; classInstance++) {
+      weClear[classInstance].classList.remove("clearOut");
     }
     setTimeout(() => {
-      for (x = 0; x < weLeave.length; x++) {
-        weLeave[x].classList.remove("iLeave");
+      for (classInstance = 0; classInstance < weLeave.length; classInstance++) {
+        weLeave[classInstance].classList.remove("iLeave");
       };
     }, 240);
   };
@@ -48,15 +48,15 @@ function clickOut() {
     document.body.classList.remove("noScroll");
     document.getElementById("navOverlay").classList.remove("navOpenOverlay");
 
-    for (x = 0; x < weFade.length; x++) {
-      weFade[x].classList.remove("fadeOut");
+    for (classInstance = 0; classInstance < weFade.length; classInstance++) {
+      weFade[classInstance].classList.remove("fadeOut");
     }
-    for (x = 0; x < weClear.length; x++) {
-      weClear[x].classList.remove("clearOut");
+    for (classInstance = 0; classInstance < weClear.length; classInstance++) {
+      weClear[classInstance].classList.remove("clearOut");
     }
     setTimeout(() => {
-      for (x = 0; x < weLeave.length; x++) {
-        weLeave[x].classList.remove("iLeave");
+      for (classInstance = 0; classInstance < weLeave.length; classInstance++) {
+        weLeave[classInstance].classList.remove("iLeave");
       };
     }, 240);
   }
