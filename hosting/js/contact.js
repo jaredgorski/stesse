c8d0504f776eb7dc1341e26b9e8a0580b933a8dc
Jared Gorski <jared.gorski@liferay.com>
2018-04-20 16:59:47 -0700

    Add WeDeploy Email Service to Contact Page - Fixes #8

---
+++ hosting/js/contact.js
@@ -0,0 +1,46 @@
+// DATA
+var form = document.querySelector('.stscontact');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  let contactData = {
+    firstName: document.querySelector('input[name="first-name"]').value,
+    lastName: document.querySelector('input[name="last-name"]').value,
+    fromEmail: document.querySelector('input[name="email-address"]').value,
+    message: document.getElementById('message').value,
+  }
+
+  // EMAIL
+  let msg = "First Name: " + contactData.firstName + "<br>Last Name: " +
+  contactData.lastName + "<br>Message: " + contactData.message;
+
+//savedata
+  WeDeploy
+    .data('stsdata-stesse.wedeploy.io')
+    .create('contact', contactData)
+    .then(function(response) {
+      form.reset();
+      console.info('Saved:', response);
+      // EMAIL post method
+      WeDeploy
+        .email('stsemail-stesse.wedeploy.io')
+        .auth('74550c77-dc21-4e9d-9908-75f2d960eb73')
+        .from(contactData.fromEmail)
+        .to('jared.gorski@liferay.com')
+        .subject("Stesse Contact - New Message")
+        .message(msg)
+        .send()
+        .then(function(response) {
+          console.log(response);
+          alert('Thank you for your message. We will get back to you shortly!');
+        })
+        .catch(function(error) {
+          console.log(error);
+          alert('There was an error. Please try again!');
+        });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
