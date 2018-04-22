cb04d288b483dcfb01dd906822448ad1bf07ccbb
Jared Gorski <jared.gorski@liferay.com>
2018-04-21 17:20:53 -0700

    Add email input box submission for gallery images - Fixes #6

--- hosting/js/photosubmit.js
+++ hosting/js/photosubmit.js
@@ -1,24 +1,20 @@
 // DATA
-var form = document.querySelector('.stscontact');
+var form = document.querySelector('.stsphotosubmit');
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  let contactData = {
-    firstName: document.querySelector('input[name="first-name"]').value,
-    lastName: document.querySelector('input[name="last-name"]').value,
-    fromEmail: document.querySelector('input[name="email-address"]').value,
-    message: document.getElementById('message').value,
+  let photoData = {
+    message: document.getElementById('photosubmit-input').value,
   }
 
   // EMAIL
-  let msg = "First Name: " + contactData.firstName + "<br>Last Name: " +
-  contactData.lastName + "<br>Message: " + contactData.message;
+  let photoSubmission = "Photo Link(s): " + photoData.message;
 
 //savedata
   WeDeploy
     .data('stsdata-stesse.wedeploy.io')
-    .create('stsdata', contactData)
+    .create('stsdata', photoData)
     .then(function(response) {
       form.reset();
       console.info('Saved:', response);
@@ -26,14 +22,14 @@ form.addEventListener('submit', function(e) {
       WeDeploy
         .email('stsemail-stesse.wedeploy.io')
         .auth('74550c77-dc21-4e9d-9908-75f2d960eb73')
-        .from(contactData.fromEmail)
+        .from(photoData.fromEmail)
         .to('jared.gorski@liferay.com')
-        .subject("Stesse Contact - New Message")
-        .message(msg)
+        .subject("Stesse - New Photo Submission")
+        .message(photoSubmission)
         .send()
         .then(function(response) {
           console.log(response);
-          alert('Thank you for your message. We will get back to you shortly!');
+          alert('Thank you for your submission!');
         })
         .catch(function(error) {
           console.log(error);
