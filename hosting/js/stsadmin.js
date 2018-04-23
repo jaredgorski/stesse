6116322a74287f5bcab9f27709b42fff4b02a3d3
Jared Gorski <jared.gorski@liferay.com>
2018-04-23 16:41:45 -0700

    Testing auth

--- hosting/js/stsadmin.js
+++ hosting/js/stsadmin.js
@@ -1,12 +1,14 @@
 
-var auth = WeDeploy.auth('auth-ststest.wedeploy.io');
+var auth = WeDeploy.auth('stsauth-ststest.wedeploy.io');
 
 
 // sign in
 function signIn() {
   auth.signInWithEmailAndPassword(user.email.value, user.password.value)
   .then(function() {
+  	console.log('yay');
     document.location.href = '../pages/stsadmin.html';
+
   })
   .catch(function() {
     alert('Sign-in failed. Try another email/password.');
@@ -20,4 +22,12 @@ function signIn() {
 if (auth.currentUser) {
     document.getElementById('name').placeholder = auth.currentUser.name;
     document.getElementById('email').placeholder = auth.currentUser.email;
+}
+
+
+function out() {
+	auth.signOut()
+	.then(() => {
+		location.href = '../pages/stsadmin-login.html';
+	});
 }
