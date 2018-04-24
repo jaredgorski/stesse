a863996d346852a21bcbd8bc554d35285d8e3432
Jared Gorski <jared.gorski@liferay.com>
2018-04-23 18:08:58 -0700

    Create admin page - Fixes #13

--- hosting/js/stsadmin.js
+++ hosting/js/stsadmin.js
@@ -1,18 +1,15 @@
-var auth = WeDeploy.auth('auth-ststest.wedeploy.io');
+var auth = WeDeploy.auth('auth-stesse.wedeploy.io');
 
 
 function signIn() {
 	console.log(user.email.value);
   auth.signInWithEmailAndPassword(user.email.value, user.password.value)
   .then(function(loginSuccess) {
-  	console.log(loginSuccess);
     document.location.href = '../pages/stsadmin.html';
-    console.log('yay');
   })
   .catch(function(loginError) {
   	console.log(loginError);
   	alert('Sign-in failed. Try another email/password.');
-    console.log('boo');
   });
 }
 
