03db61cf60406ced52aa578b291abddc5db5b305
Jared Gorski <jared.gorski@liferay.com>
2018-04-24 17:52:02 -0700

    Move photo validation to the submission action

--- hosting/js/photopopulate.js
+++ hosting/js/photopopulate.js
@@ -15,11 +15,7 @@ function populateImages(photoData) {
   var galleryGrid = '';
 
   photoData.forEach(function(image) {
-    var imageUrl = image.message;
-    var goodImage = imageUrl.includes(".jpg") || imageUrl.includes(".jpeg");
-    if (goodImage) {
     galleryGrid += `<div class="image-grid-content" id="gridImage" style="background-image:url(${image.message});"></div>`;
-    }
   });
 
   imgGridContent.innerHTML = galleryGrid;
--- hosting/js/photosubmit.js
+++ hosting/js/photosubmit.js
@@ -12,31 +12,38 @@ form.addEventListener('submit', function(e) {
   let photoSubmission = "Photo Link(s): " + photoData.message;
 
 //savedata
-  WeDeploy
-    .data('stsdata-stesse.wedeploy.io')
-    .create('stsdataPhotos', photoData)
-    .then(function(response) {
-      form.reset();
-      console.info('Saved:', response);
-      // EMAIL post method
-      WeDeploy
-        .email('stsemail-stesse.wedeploy.io')
-        .auth('74550c77-dc21-4e9d-9908-75f2d960eb73')
-        .from('jared.gorski@wedeploy.com')
-        .to('jared.gorski@liferay.com')
-        .subject("Stesse - New Photo Submission")
-        .message(photoSubmission)
-        .send()
-        .then(function(response) {
-          console.log(response);
-          alert('Thank you for your submission!');
-        })
-        .catch(function(error) {
-          console.log(error);
-          alert('There was an error. Please try again!');
-        });
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+  var imageUrl = photoData.message;
+  var goodImage = imageUrl.includes(".jpg") || imageUrl.includes(".jpeg");
+  if (goodImage) {
+    WeDeploy
+      .data('stsdata-stesse.wedeploy.io')
+      .create('stsdataPhotos', photoData)
+      .then(function(response) {
+        form.reset();
+        console.info('Saved:', response);
+        // EMAIL post method
+        WeDeploy
+          .email('stsemail-stesse.wedeploy.io')
+          .auth('74550c77-dc21-4e9d-9908-75f2d960eb73')
+          .from('jared.gorski@wedeploy.com')
+          .to('jared.gorski@liferay.com')
+          .subject("Stesse - New Photo Submission")
+          .message(photoSubmission)
+          .send()
+          .then(function(response) {
+            console.log(response);
+            
+            alert('Thank you for your submission!');
+          })
+          .catch(function(error) {
+            console.log(error);
+            alert('There was an error. Please try again!');
+          });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  } else {
+    alert('Only direct urls to .jpg images will be accepted. Please check your url and try again.')
+  }
 });
