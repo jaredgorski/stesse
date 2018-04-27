65fc41380834964d73abad4af973acaec801e8b8
Jared Gorski <jared.gorski@liferay.com>
2018-04-27 16:20:21 -0700

    Make products editable via admin

--- admin/index.js
+++ admin/index.js
@@ -8,6 +8,7 @@ const storage = multer.diskStorage({
 })
 const upload = multer({storage});
 const app = express();
+
 app.post('/upload', upload.single('productImage'), (req, res) => {
   WeDeploy
     .data('stsdata-stesse.wedeploy.io')
@@ -24,21 +25,24 @@ app.post('/upload', upload.single('productImage'), (req, res) => {
       res.redirect(`${req.protocol}://${req.get('host')}?cmd=fail`);
     });
 });
+
+app.post('/update', upload.single('productImage'), (req, res) => {
+  WeDeploy
+    .data('stsdata-stesse.wedeploy.io')
+    .update('stsdataProducts', req.body.productId, {
+      "title": req.body.productTitle,
+      "price": req.body.productPrice,
+      "link": req.body.productLink,
+      "image": req.file.filename
+    })
+    .then(function(succ){
+      res.redirect(`${req.protocol}://${req.get('host')}?cmd=success`);
+    })
+    .catch(function(err){
+      res.redirect(`${req.protocol}://${req.get('host')}?cmd=fail`);
+    });
+});
+
 app.use(express.static(__dirname + '/public'));
 app.use(serveIndex('public', {'icons': true}));
 app.listen(3000);
-
-
-
-// function updateProduct() {
-//   var prodUpdateId = document.getElementById('produpdate-id').value;
-//   var photoUpdateTitle = document.getElementById('produpdate-link').value;
-//   var photoUpdateLink = document.getElementById('produpdate-link').value;
-//     WeDeploy
-//       .data('https://stsdata-stesse.wedeploy.io')
-//       .update('stsdataPhotos/' + photoUpdateId, {
-//         "message": photoUpdateUrl
-//       }).then(function(success) {
-//         alert("Gallery updated!");
-//       });
-// }
