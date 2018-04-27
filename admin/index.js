4bd4877c41dc5a1df0c39a163e17af26bf816185
Jared Gorski <jared.gorski@liferay.com>
2018-04-27 12:35:43 -0700

    Create admin nano service

---
+++ admin/index.js
@@ -0,0 +1,29 @@
+const serveIndex = require('serve-index');
+const express = require('express');
+const WeDeploy = require('wedeploy');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => cb(null, file.originalname),
+  destination: (req, file, cb) => cb(null, './public/uploads'),
+})
+const upload = multer({storage});
+const app = express();
+app.post('/upload', upload.single('productImage'), (req, res) => {
+  WeDeploy
+    .data('stsdata-stesse.wedeploy.io')
+    .create('stsdataProducts', {
+      "title": req.body.productTitle,
+      "price": req.body.productPrice,
+      "link": req.body.productLink,
+      "image": req.file.filename
+    })
+    .then(function(succ){
+      res.redirect(`${req.protocol}://${req.get('host')}?cmd=success`);
+    })
+    .catch(function(err){
+      res.redirect(`${req.protocol}://${req.get('host')}?cmd=fail`);
+    });
+});
+app.use(express.static(__dirname + '/public'));
+app.use(serveIndex('public', {'icons': true}));
+app.listen(3000);
